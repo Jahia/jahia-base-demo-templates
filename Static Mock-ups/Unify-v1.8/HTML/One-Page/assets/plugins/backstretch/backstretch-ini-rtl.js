d102b45e177e87abbac5a6dc867f3860c1c475d1
ergener <ergener@gmail.com>
2016-02-19 15:20:58 -0500

    Remove Static Mock-ups

--- Static Mock-ups/Unify-v1.8/HTML/One-Page/assets/plugins/backstretch/backstretch-ini-rtl.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $.getScript("../assets/plugins/backstretch/jquery.backstretch.min.js", function() {
-    $(".fullscreen-static-image").backstretch("../assets/img/bg/img1.jpg");
-  });
-});
--- Static Mock-ups/Unify-v1.8/HTML/One-Page/assets/plugins/wow/wow-ini.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$.getScript("assets/plugins/wow/dist/wow.min.js", function(){
-		new WOW().init();
-	});
-});
