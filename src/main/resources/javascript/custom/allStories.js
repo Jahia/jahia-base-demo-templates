cadd840bd62d226a553e443d115e9529c6afe321
fabrice aissah <faissah@jahia.com>
2016-04-26 13:59:17 -0400

    code review: fix allStories ajax loading

--- src/main/resources/javascript/custom/allStories.js
+++ src/main/resources/javascript/custom/allStories.js
@@ -1,6 +1,9 @@
 
 function activePageViewTab() {
     var allStoriesTab = GetURLParameter('allStoriesTab');
+    if (allStoriesTab == null){
+        allStoriesTab = "all";
+    }
     $(".allStories").find("." + allStoriesTab).addClass("active");
 }
 
@@ -9,20 +12,15 @@ function activateStoriesTabAjax(){
         var view = $(this).attr('view');
         history.pushState(null, null, window.location.href.split('?')[0] + '?allStoriesTab=' + view);
         var allStories = $(this).closest(".allStories");
-        allStories.parent().load(allStories.attr('url') + '.ajax?allStoriesTab=' + view+ '#'+allStories.attr('id'));
+        allStories.parent().load(allStories.attr('url') + '.ajax?allStoriesTab=' + view+ ' #'+allStories.attr('id'), function() {
+            activateStoriesTabAjax();
+            activePageViewTab();
+        });
         activePageViewTab();
-        $(this).closest(".allStories")
     });
 }
 
 $(document).ready(function () {
-    // Initialize to first tab being active
-    $(".allStories").find("." + "all").addClass("active");
-    // After an ajax call has been done, we need to rerun the JS on the HTML fragment that has been reloaded
-    $(document).bind('DOMNodeInserted', function (e) {
-        activePageViewTab();
-        activateStoriesTabAjax();
-    });
-
     activateStoriesTabAjax();
+    activePageViewTab();
 })
