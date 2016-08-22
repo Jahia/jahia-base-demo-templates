cf8f9d63b955152b25027eebab69e048dc371737
fabrice aissah <faissah@jahia.com>
2016-08-22 18:18:52 -0400

    Separate jahia demo components and template set

--- src/main/resources/javascript/custom/slider.js
+++
@@ -1,8 +0,0 @@
-$(window).load(function () {
-    var sliders = document.querySelectorAll(".master-slider-jahia");
-
-    for (var i = 0; i < sliders.length; i++) {
-
-        MSfullWidth.initMSfullWidth(sliders[i].id, sliders[i].getAttribute('transition'), sliders[i].getAttribute('layout'), sliders[i].getAttribute('autoplay')=="true", sliders[i].getAttribute('panelcount'));
-    }
-});
